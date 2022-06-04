fecc53b3fb115c50e2a36661b069055efba99273
alesiobitaGit <alesio100.ab@gmail.com>
2022-06-04 12:21:10 +0200

    provider update

--- provider.tf
+++ provider.tf
@@ -1,18 +1,12 @@
 terraform {
   required_version = ">=1.0.0"
-   aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
 }
-/*
+
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
 }
-*/
+/*
 cloud {
     organization = "My_ProjectJenkings"
 
@@ -20,3 +14,4 @@ cloud {
       name = "gh-actions-jenkings"
     }
 }
+*/
